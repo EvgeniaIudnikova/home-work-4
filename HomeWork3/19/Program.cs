5e311080b9ba3084287693a2b330e9bcf2917270
«EVGENIYA» <ev.postnova@yandex.ru>
2022-12-07 10:20:09 +0300

    19 задача

--- HomeWork3/19/Program.cs
+++ HomeWork3/19/Program.cs
@@ -2,3 +2,24 @@
 // и проверяет, является ли оно палиндромом.
 // 14212 -> нет
 // 12821 -> да
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+string s = Console.ReadLine();
+int length = s.Length;
+int i = 0;
+int j = length-1;
+while (i<j)
+{
+    if (s[i] == s[j])
+    {
+        i++;
+        j = j-1;
+    }
+    else 
+    {
+        Console.WriteLine($"число {s} не палиндром");
+        return;
+    }
+}
+ Console.WriteLine($"число {s} - палиндром");
