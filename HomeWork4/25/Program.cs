87e15fb7f6ee582915d1a7ca10950a137b2ddd1a
«EVGENIYA» <ev.postnova@yandex.ru>
2022-12-07 23:09:46 +0300

    25 задача

---
+++ HomeWork4/25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход 
+// два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+Console.Write("Введите число А: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите число В: ");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine($"{A} в {B} степени = {MetodPow(A, B)}");
+
+int MetodPow (int number1, int number2)
+{
+    int count = 1;
+    int Multi = 1;
+    while (count <= B)
+    {
+        Multi *= A;
+        count++;
+    }
+    return Multi;
+}
