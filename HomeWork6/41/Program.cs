e4fab8097f0ebc390f9a653eeae3df5511e6de1a
«EVGENIYA» <ev.postnova@yandex.ru>
2022-12-10 17:56:13 +0300

    домашняя работа 6

---
+++ HomeWork6/41/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.WriteLine("Введите в строку числа через пробел");
+string numbers = Console.ReadLine();
+int[] num = NewMass(numbers);
+Console.Write($"В массиве [{String.Join(", ", num)}] положительных чисел в массиве = {CountPlus(num)}");
+
+//функция принимает строку и переводит ее в массив строк, а затем массив строк переводит в массив целых чисел
+int[] NewMass (string m)
+{
+    string[] now = m.Split(" ", System.StringSplitOptions.RemoveEmptyEntries);
+    int[] array = new int[now.Length];
+    for (int i= 0; i< now.Length; i++)
+    {
+        array[i] = int.Parse(now[i]);
+    }
+return array;
+}
+
+//фу-ция считающая кол-во положительных элементов в массиве 
+int CountPlus (int[] arr)
+{
+    int count = 0;
+    foreach (int el in arr)
+    {
+        if (el > 0 )
+        {
+            count++;  
+        }
+    }
+return count;
+}
---
+++ HomeWork6/44/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения
+// двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+Console.WriteLine("Даны две прямые заданные уравнениями y = k1 * x + b1 ;  y = k2 * x + b2");
+Console.WriteLine("Введите значение b1");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение k1");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение b2");
+double b2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение k2");
+double k2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"точка пересечения прямых ({String.Join("; " ,ToLine (b1, k1, b2, k2))})");
+
+//Функция высчитывает точку пересечения двух прямых заданных уравнениями
+//y = k1 * x + b1, y = k2 * x + b2;
+double[] ToLine (double B1, double K1, double B2, double K2)
+{
+double k = k1+k2;
+double b = -1*(b1+b2);
+double x = b / k;
+double y = k1 * x + b1;
+double[] coordinates = new double [2];
+coordinates [0] = x;
+coordinates [1] = y;
+return coordinates;
+}
+
+//можно без функции просто стр 23 - 26 и ввод следующей стр
+//Console.WriteLine($"точка пересечения прямых ({x:f3}; {y:f3})");
+
