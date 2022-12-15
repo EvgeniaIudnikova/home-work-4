99250df27f6bbc358d6cbb65dd4381b79ffde88b
«EVGENIYA» <ev.postnova@yandex.ru>
2022-12-15 21:19:47 +0300

    60 задача

---
+++ HomeWork8/60/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+//
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+Console.Clear();
+Console.Write("Введите x: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите y: ");
+int y = int.Parse(Console.ReadLine());
+Console.Write("Введите z: ");
+int z = int.Parse(Console.ReadLine());
+int[,,] matrix = GetArray(x, y, z, 0, 10);
+PrintArray(matrix);
+Console.WriteLine();
+
+int[,,] GetArray(int x, int y, int z, int min, int max)
+{
+    int[,,] array = new int[x, y, z];
+    for (int i = 0; i < x ; i++)
+    {    
+        for (int j = 0; j < y; j++)
+        {
+            for (int k = 0; k < z; k++)
+            {
+                array[i, j, k] = new Random().Next(min, max + 1);
+            }
+        }    
+    }
+    return array;        
+}
+
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]} [{i}, {j}, {k}]   ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
